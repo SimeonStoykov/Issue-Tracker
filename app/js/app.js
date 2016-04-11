e78559c7263c401f50f90f37e4db0b46d8036a47
Simeon Stoykov <simeon.stoykov@Softwaregroup-bg.com>
2016-04-11 17:47:30 +0300

    Added login and register forms

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,4 @@
-var issueTrackerApp = angular.module('issueTracker', ['ngRoute'])
+var issueTracker = angular.module('issueTracker', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/register', {
-            templateUrl: 'views/register.html',
-            controller: 'MainController'
-        });
         $routeProvider.otherwise({ redirectTo: '/' });
     }]);
